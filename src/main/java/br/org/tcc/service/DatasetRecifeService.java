f27e53b16b1f22b428c2f3c7481d01b1b47d7119
Marcelo VItório <marcelo.araujo@govbr.com.br>
2020-09-29 00:14:14 -0300

    fix build

--- src/main/java/br/org/tcc/service/DatasetRecifeService.java
+++ src/main/java/br/org/tcc/service/DatasetRecifeService.java
@@ -2,6 +2,7 @@ package br.org.tcc.service;
 
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -14,6 +15,7 @@ import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
+@ApplicationScoped
 public interface DatasetRecifeService {
 
     /**
